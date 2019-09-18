9f8dc701569a75463b52adcb180348c88b97acd6
Edwin Chajon <junchajo@gmail.com>
2019-09-18 14:45:40 -0700

    this

--- assignments/this.js
+++ assignments/this.js
@@ -10,17 +10,46 @@
 */
 
 // Principle 1
-
 // code example for Window Binding
 
-// Principle 2
+window.number = 27
+function num () {
+  console.log(`My age is ${this.number}`)
+}
+num()
 
+
+// Principle 2
 // code example for Implicit Binding
 
-// Principle 3
+const me = {
+    name: 'Edwin',
+    greet() {
+      console.log(`Hello, my name is ${this.name}`)
+    }
+  }
+  me.greet();
 
+
+// Principle 3
 // code example for New Binding
 
-// Principle 4
+function newUser (name, age) {
+    this.name = name
+    this.age = age
+  }
+  
+  const person = new newUser('Edwin', 25)
+  console.log(person)
 
+
+// Principle 4
 // code example for Explicit Binding
+
+function greeting () {
+    console.log(`Hello, my name is ${this.name}`)
+  }
+  const me = {
+    name: 'Edwin',
+  }
+  greeting.call(me)
